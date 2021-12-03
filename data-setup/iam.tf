68b8d13eee7ad8c91b37619f2a13ce3dd650b8ea
Kelsey Mok <kelseymok@gmail.com>
2021-12-03 15:24:37 +0100

    km: adding the right permissions to read glue tables and s3 bucket from trino reader

--- data-setup/iam.tf
+++ data-setup/iam.tf
@@ -17,4 +17,35 @@ data "aws_iam_policy_document" "reader-assume" {
   }
 }
 
+data "aws_iam_policy_document" "reader-read" {
+  statement {
+    actions = [
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetDatabase",
+      "glue:GetDataBases"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion"
+    ]
+    resources = [
+      "${aws_s3_bucket.data.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "reader-read"  {
+  policy = data.aws_iam_policy_document.reader-read.json
+}
+
+resource "aws_iam_role_policy_attachment" "reader-read" {
+  policy_arn = aws_iam_policy.reader-read.arn
+  role       = aws_iam_role.this.id
+}
+
 data "aws_caller_identity" "this" {}
